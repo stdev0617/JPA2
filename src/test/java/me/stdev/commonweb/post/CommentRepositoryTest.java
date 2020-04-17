a2e7fcc2cdf00a4b543ed14109880805f23e4f64
stdev0617 <stdev0617@gmail.com>
2020-04-17 23:43:32 +0900

    query by example

--- src/test/java/me/stdev/commonweb/post/CommentRepositoryTest.java
+++ src/test/java/me/stdev/commonweb/post/CommentRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -37,7 +39,6 @@ public class CommentRepositoryTest {
 //        commentRepository.save(comment);
         comment.setUp(10);
         comment.setDown(1);
-        comments.save(comment);
 
 //        comments.findByPost_Id(savedPost.getId()).forEach(c -> {
 //            System.out.println("=================");
@@ -62,5 +63,17 @@ public class CommentRepositoryTest {
     public void specs() {
         //comments.findAll(CommentSpecs.isBest().or(CommentSpecs.isGood()));
         Page<Comment> page = comments.findAll(isBest().or(isGood()), PageRequest.of(0,10));
+    }
+
+    @Test
+    public void qbe() {
+        Comment prove = new Comment();
+        prove.setBest(true);
+
+        ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny();
+
+        Example<Comment> example = Example.of(prove, exampleMatcher);
+
+        comments.findAll(example);
     }
 }
