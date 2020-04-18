583a23ba61f95a58530604b728eb779f0353edf8
stdev0617 <stdev0617@gmail.com>
2020-04-19 00:59:47 +0900

    auditing

--- src/main/java/me/stdev/commonweb/post/Comment.java
+++ src/main/java/me/stdev/commonweb/post/Comment.java
@@ -1,10 +1,18 @@
 package me.stdev.commonweb.post;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @NamedEntityGraph(name = "Comment.post",
         attributeNodes = @NamedAttributeNode("post")) // post데이터를 eager로 가져오려면 이걸로 연관관계를 정의함.
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Comment {
 
     @Id
@@ -21,6 +29,20 @@ public class Comment {
     private int down;
 
     private boolean best;
+
+    @CreatedDate
+    private Date created;
+
+    @CreatedBy
+    @ManyToOne
+    private Account createdBy;
+
+    @LastModifiedDate
+    private Date updated;
+
+    @LastModifiedBy
+    @ManyToOne
+    private Account updatedBy;
 
     public Long getId() {
         return id;
@@ -68,5 +90,10 @@ public class Comment {
 
     public void setBest(boolean best) {
         this.best = best;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        System.out.println("Pre Persist is called");
     }
 }
--- src/main/java/me/stdev/commonweb/post/CommentRepository.java
+++ src/main/java/me/stdev/commonweb/post/CommentRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -16,5 +18,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpec
 
     //List<CommentSummary> findByPost_Id(Long id);
 
+    @Transactional(readOnly = true)
     <T> List<T> findByPost_Id(Long id, Class<T> type);
 }
